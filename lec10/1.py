bbca41a7c5f3067ef8cfc3dc01e571cdbe709154
Faiyaz Rafeek <fayaznahwi@gmail.com>
2022-07-05 18:00:09 +0530

    Create 1.py

---
+++ lec10/1.py
@@ -0,0 +1,33 @@
+#python comment
+
+print("Hello world")
+
+"""
+This 
+is 
+Python
+multiline
+comment
+"""
+
+print(type(5))
+
+val = len([56,35,9,6])
+
+print(max(15,6,9))
+
+print(str(56))
+
+print(int('50') + 3)
+
+"hello"
+
+print('very' +'hot')
+
+#print('7'+2)
+
+print(10* 'yes')
+
+width = 10
+
+print(width)
